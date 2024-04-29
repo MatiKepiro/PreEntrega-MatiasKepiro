170843361cabe2da5bd78de89ca1d531d3ce3b99
Matias Kepiro <matias.kepiro@gmail.com>
2024-04-29 16:51:23 -0300

    Cambios Re-Entrega (PreEntrega 2)

--- main.js
+++ main.js
@@ -1,147 +1,187 @@
 // Array Principal
-let playlist = []
+const playlist = []
+const elementoPrincipal = document.getElementById("main")
+const setFisico = document.getElementById("setFisico")
+const botonSet = document.getElementById("set")
+const cancionesDOM = document.getElementById("canciones")
 
+
+/*
 // Variables
 let numeroDeCanciones = 0, bandera = true, nombreDeCancion, existe, nombre
+*/
 
-
-// Funcion para sincronizar el array con el Local Storage
-const recuperarPlaylist = () => {
-    // Vacio el array
-    playlist = []
-    numeroDeCanciones = 0
-    // Ciclo for que itera sobre la longitud del Local Storage y va agregando los elementos ya almacenados anteriormente
-    for (let i = 1; i <= localStorage.length + 1; i++) {
-        // Variable para conseguir el nombre de la cancion
-        nombre = localStorage.getItem("Cancion " + i)
-        // En caso de eliminar un elemento (que apareceria como null), simplemente lo salteamos y seguimos iterando el proximo elemento
-        if (nombre !== null) {
-            // Metodo push para agregar el elemento al array
-            numeroDeCanciones++
-            playlist.push({numero: numeroDeCanciones, nombre: nombre})
-        }
+// Inicio Clase
+// Clase para toda la logica del Set de Playlist (Las canciones guardadas)
+class SetDePlaylist {
+    constructor() {
+        // Constructor para crear obtener las canciones
+        this.canciones = JSON.parse(localStorage.getItem('set')) || []
     }
-    // Invoco a la funcion para actualizar el Local Storage
-    limpiarStorage(playlist)
-}
 
-
-// Funcion para limpiar y volver a almacenar los elementos que se encuentran dentro del array
-const limpiarStorage = (array) => {
-    localStorage.clear()
-    for (const i of array) {
-        localStorage.setItem("Cancion " + i.numero, i.nombre)
-    }
-}
-
-// Condicional para verificar si el Local Storage esta vacio o no. Si lo esta, no invocamos la funcion.
-if (localStorage.length > 0) {
-    recuperarPlaylist()
-}
-
-
-
-// Funcion para agregar una cancion al array
-// Parametros: (numero de la ultima cancion, nombre de la cancion a agregar)
-const agregarCancion = (numero, cancion) => {
-    // Agregamos +1 para sumar una cancion
-    numeroDeCanciones++
-    // Agrega el elemento al array
-    playlist.push({numero: numero + 1, nombre: cancion})
-    // Agrega el elemento al Local Storage
-    localStorage.setItem("Cancion " + numeroDeCanciones, cancion)
-}
-
-
-// Funcion para Eliminar Cancion (en esta se me complico mucho, por el tema que lo elimina pero lo deja como null...)
-// Parametros: (nombre de la cancion, array donde almacena las canciones)
-const eliminarCancion = (cancion, array) => {
-    // Metodo .find() para buscar el objeto dentro del array. Comparandolo con la cancion ingresada por el usuario
-    let objeto = array.find(el => ((el.nombre).toLowerCase() === cancion.toLowerCase()))
-    // Agarramos unicamente el numero del objeto (key)
-    let numeroDeCancion = objeto.numero
-    // Eliminamos el elemento del Local Storage
-    localStorage.removeItem("Cancion " + numeroDeCancion)
-    // Actualizamos el array para descartar el objeto eliminado
-    recuperarPlaylist()
-}
-
-
-
-// Contenedor Princial
-const divContenedorPricipal = document.createElement("div")
-const titulo = document.createElement("h1")
-const botonDarkMode = document.createElement("div")
-const contenedorSecundario = document.createElement("h2")
-
-// Asociamos los nodos hijos con el padre (divContenedorPricipal)
-divContenedorPricipal.appendChild(titulo)
-divContenedorPricipal.appendChild(botonDarkMode)
-divContenedorPricipal.appendChild(contenedorSecundario)
-
-
-// div contenedor
-const contenedor = document.getElementById("contenedor")
-contenedor.appendChild(divContenedorPricipal)
-
-
-// Fondo
-const fondoPrincipal = document.getElementById("fondo")
-
-
-
-// Titulo Principal
-titulo.innerText = "Simulador de Playlist"
-titulo.classList.add("titulo")
-
-
-
-// Boton Dark Mode
-botonDarkMode.innerHTML = "<input type='button' value='Dark Mode'>"
-botonDarkMode.addEventListener("click", respuestaClick = () => {
-    fondo.classList.toggle("black")
-    titulo.classList.toggle("textWhite")
-    contenedorSecundario.classList.toggle("textWhite")
-})
-
-
-// Mi Playlist: + Botones de "Agregar" y "Eliminar"
-contenedorSecundario.innerHTML = "Mi Playlist: <br><input id='botonAgregar'type='button' value='Agregar'><br><input id='botonEliminar'type='button' value='Eliminar'>"
-const botonAgregar = document.getElementById("botonAgregar")
-const botonEliminar = document.getElementById("botonEliminar")
-
-// Evento Boton "Agregar"
-botonAgregar.addEventListener("click", respuestaClick = () => {
-    
-    // Condicional para ver si quiere agregar una cancion mas
-    if (confirm("Desea agregar una cancion?")) {
-        // Pedimos el nombre de la cancion mediante un "prompt"
-        nombreDeCancion = prompt("Nombre de la cancion:")
-        // Condicional para chequear si existe la cancion.
-        // Si existe muestra un mensaje que avisa que ya estaba agregada.
-        if (playlist.find(el => ((el.nombre).toLowerCase() === nombreDeCancion.toLowerCase()))) {
-            alert("Ya ha sido agregada anteriormente!")
+    // Funcion para agregar un cancion
+    agregarCancion(cancion) {
+        // Chequeo si existe la cancion o no
+        const cancionExistente = this.canciones.find(e => e.nombre === cancion.nombre)
+        
+        //  Si existe, le agregamos +1 a la cantidad
+        if (cancionExistente) {
+            cancionExistente.cantidad++
         }
-        // Si no existe, la agrega al array y al localStorage
+        // Si no existe, la agregamos
         else {
-            agregarCancion(numeroDeCanciones, nombreDeCancion)
-            alert("Cancion agregada!")
+            cancion.cantidad = 1
+            this.canciones.push(cancion)
         }
+
+    // Actualizamos el Local Storage
+    this.guardarSet()
+    
+    } 
+
+    // Guardamos las canciones en el Local Storage como 'set'
+    guardarSet() {
+        localStorage.setItem('set', JSON.stringify(this.canciones))
     }
+
+
+
+    // Borramos los datos del Local Storage y del array
+    limpiarSet() {
+        localStorage.removeItem('set')
+        this.canciones = []
+    }
+}
+// Fin Clase
+
+
+// Creamos el Set de Playlist
+const miSet = new SetDePlaylist()
+
+
+
+// Funcion para crear la tarjeta de cada cancion
+// Entrando como parametros, 1. Nombre 2. Autor 3. Imagen.
+function crearTarjetaDeCancion({nombre, autor, imagen}) {
+    elementoPrincipal.innerHTML += `
+        <div class= "contenedorImagen">
+            <img src="${imagen}" />
+            <div>
+                <div>
+                    <h3 class="nombre">${nombre}</h3>
+                    <p class="autor">${autor}<p/>
+                </div>
+                <div class="contenedorBoton">
+                    <button id="colorBoton" class="agregar">Agregar</button>
+                </div>
+            </div>
+        </div>
+    `
+}
+
+
+fetch("./info.json")
+.then(datos => {
+    if(!datos.ok) {
+        throw new Error("Error al traer los datos")
+    } else {
+        return datos.json()
+    }
+})
+.then(elementos => {
+    elementos.canciones.forEach(elemento => {
+        crearTarjetaDeCancion(elemento)
+    });
+    agregarEvento()
+})
+.catch(e => {
+    console.error("Hubo un error al operar el fetch " + e.message)
 })
 
 
-// Evento Boton "Eliminar"
-botonEliminar.addEventListener("click", respuestaClick = () => {
-    // Pedimos el nombre mediante un "prompt"
-    nombreDeCancionAEliminar = prompt("Nombre de cancion:")
-    // Condicional para chequear la igualdad entre la cancion ingresada por el usuario y las canciones almacenadas en el array
-    if (playlist.find(el => ((el.nombre).toLowerCase() === nombreDeCancionAEliminar.toLowerCase()))) {
-        // Invocamos la funcion para eliminar la cancion y mostramos un mensaje
-        eliminarCancion(nombreDeCancionAEliminar, playlist)
-        alert("Cancion eliminada")
-    } else {
-        // En caso contrario, muestra un mensaje de advertencia
-        alert("No existe esa cancion dentro de la Playlist")
-    }
+// Mostramos el Set de Playlist
+// Funcion general
+function mostrarSet() {
+
+    // Vaciamos el HTML del Set Fisico
+    setFisico.innerHTML = ""
+
+    // Creamos el nuevo array
+    miSet.canciones.map(cancion => {
+        setFisico.innerHTML += `
+        <div class="cancionEnSet">
+            <p class="nombreCancion">${cancion.nombre}</p>
+            <div class="cantidad">
+                <p>${cancion.cantidad}</p>
+            </div>
+        </div>`
+    })
+
+    // Agregamos el boton de "borrar" y "cancelar"
+    setFisico.innerHTML += `
+    <div>
+        <button id="borrar">Terminar Playlist</button>
+        <button id="cancelar"> Cancelar Playlist</button>
+    </div>`
+
+    // Agregamos evento al boton "cancelar"
+    const cancelar = document.getElementById("cancelar")
+    // Evento para cancelar la creacion de la playlist
+    cancelar.addEventListener("click", () => {
+        miSet.limpiarSet()
+        Swal.fire({
+            icon: "error",
+            title: "Su Playlist fue cancelada",
+        })
+        mostrarSet()
+    })
+
+
+    // Agreagamos evento al boton "eliminar"
+    const borrar = document.getElementById("borrar")
+    // Evento para eliminar y vaciar la playlist terminada
+    borrar.addEventListener("click", () => {
+        miSet.limpiarSet()
+        Swal.fire({
+            title: "Playlist terminada!",
+            text: "Su playlist fue creada con exito!",
+            icon: "success"
+        })
+        mostrarSet()
+    })
+}
+
+
+// Funcion para agregar el evento "agregar" a cada boton
+function agregarEvento() {
+    // Buscamos el boton de agregar
+    const botones = document.getElementsByClassName("agregar")
+    // Lo transformamos en un array
+    const arrayBotones = Array.from(botones)
+
+    // Metodo .map para que por cada boton le agrege un evento "click"
+    arrayBotones.map(boton => {
+        boton.addEventListener("click", e => {
+            let nombre = e.target.parentElement.parentElement.children[0].children[0].innerText
+            // Agregamos la cancion al set
+            miSet.agregarCancion({
+                nombre,
+                cantidad: 1,
+            })
+
+            // Mostramos una notificacion de exito
+            Swal.fire({
+                title: `¡Su cancion ${nombre} se agrego correctamente!`,
+                icon: "success",
+                width: "350px"
+            });
+
+            mostrarSet()
+        })
+    })
+}
+
+// Agregamos evento al set para que se active o desactive cada vez que clickeamos para ver el set
+botonSet.addEventListener("click", () => {
+    setFisico.classList.toggle(`active`)
 })
